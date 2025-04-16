e91d5a2d4f62b6844e38a575191bdc4bae971108
Dante Terceros <tercerosdante@gmail.com>
2025-04-16 09:54:28 -0400

    Add basic react + vite app

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>JourBuddy</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
