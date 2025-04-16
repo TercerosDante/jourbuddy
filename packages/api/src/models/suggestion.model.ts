8939331dd2c9bed5c776514374f2981f90c7262b
Dante Terceros <tercerosdante@gmail.com>
2025-04-16 11:08:44 -0400

    Move suggestion search logic to service

--- packages/api/src/models/suggestion.model.ts
+++ packages/api/src/models/suggestion.model.ts
@@ -8,19 +8,7 @@ type SuggestionItem = {
 const data: SuggestionItem[] = suggestionData;
 
 export const Suggestion = {
-  generate: (prompt: string) => {
-    const normalizedPrompt = prompt.toLowerCase();
-
-    const found = data.find((item) => {
-      return item.keywords.some((keyword) => normalizedPrompt.includes(keyword.toLowerCase()));
-    });
-
-    if (!found) {
-      return [];
-    }
-
-    const itemsToReturn = Math.floor(Math.random() * 3) + 3;
-    const shuffledSuggestions = [...found.suggestions].sort(() => Math.random() - 0.5);
-    return shuffledSuggestions.slice(0, itemsToReturn);
+  getDB: () => {
+    return data;
   },
 };
--- packages/api/src/services/suggestions.service.ts
+++ packages/api/src/services/suggestions.service.ts
@@ -2,7 +2,17 @@ import { Suggestion } from '../models/suggestion.model';
 
 const SuggestionService = {
   generate: async (prompt: string) => {
-    return Suggestion.generate(prompt);
+    const normalizedPrompt = prompt.toLowerCase();
+    const data = Suggestion.getDB();
+    const found = data.find((item) => {
+      return item.keywords.some((keyword) => normalizedPrompt.includes(keyword.toLowerCase()));
+    });
+    if (!found) {
+      return [];
+    }
+    const itemsToReturn = Math.floor(Math.random() * 3) + 3;
+    const shuffledSuggestions = [...found.suggestions].sort(() => Math.random() - 0.5);
+    return shuffledSuggestions.slice(0, itemsToReturn);
   },
 };
 
