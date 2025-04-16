485a0112062acef6f108b4f0dc3cd23751889dc6
Dante Terceros <tercerosdante@gmail.com>
2025-04-16 08:41:35 -0400

    Add simple body request schema validation

--- packages/api/src/routes/suggestions.routes.ts
+++ packages/api/src/routes/suggestions.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import SuggestionController from '../controllers/suggestions.controller';
-
+import { validate } from '../common/middleware/validation.middleware';
+import { promptSchema } from '../common/schemas/prompt.schema';
 const router = Router();
 
-router.post('/', SuggestionController.generate);
+router.post('/', validate(promptSchema), SuggestionController.generate);
 
 export default router;
