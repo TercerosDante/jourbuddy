3404d445d633e43262090e138432d6e8cb845428
Dante Terceros <tercerosdante@gmail.com>
2025-04-16 10:24:38 -0400

    Add tailwind

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,14 +1,9 @@
 function App() {
   return (
-    <div className="app">
-      <h1>JourBuddy</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <h1 className="text-3xl font-bold text-center mb-8 text-gray-900">JourBuddy</h1>
     </div>
   );
-};
+}
 
 export default App;
